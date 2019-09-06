856596d1a9c0cc2fcfff0095ce5db4d6cd31fa79
unknown <ref>
2019-09-06 15:25:58 +0530

    Blogs Working

---
+++ packages/blog/src/layouts/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { PrimaryLayout } from "@vinayg-cp/shared-ui"
+const Layout = ({ children, data }) => (
+  <PrimaryLayout children={children} data={data} />
+)
+Layout.propTypes = {
+  children: PropTypes.func,
+}
+export default Layout
+export const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }`
