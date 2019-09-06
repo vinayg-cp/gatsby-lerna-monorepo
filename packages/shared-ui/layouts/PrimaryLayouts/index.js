656963e5fcc1c29412cab989206d4297dad13ebb
unknown <ref>
2019-09-06 14:38:09 +0530

    Before making shared-ui a package

---
+++ packages/shared-ui/layouts/PrimaryLayouts/index.js
@@ -0,0 +1,5 @@
+export { default as theme } from "./theme"
+export { default as Button } from "./components/Button"
+export { default as Header } from "./components/Header"
+export { ItemCard, ItemCardWrapper } from "./components/ItemCard"
+export { default as PrimaryLayout } from "./layouts/PrimaryLayouts"
